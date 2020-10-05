f6e16dac9f242557cd5d64ea75f6fdc33287b171
MaeglynD <maeglyn.davies@gmail.com>
2020-10-05 22:31:14 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
 import Vue from 'vue';
-import * as VueGoogleMaps from 'vue2-google-maps';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
-
-Vue.use(VueGoogleMaps, {
-	load: {
-		key: 'AIzaSyC52wPqOEjpGIK1E4olE3be5rgqXHtx8s8',
-		libraries: 'places', // This is required if you use the Autocomplete plugin
-		// OR: libraries: 'places,drawing'
-		// OR: libraries: 'places,drawing,visualization'
-	},
-});
 
 Vue.config.productionTip = false;
 
